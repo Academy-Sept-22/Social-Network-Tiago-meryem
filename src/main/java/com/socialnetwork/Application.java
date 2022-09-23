26ce2a4a152ea8a08a91e9b4f3c6ab3847c5dca7
tiagobica <tiago.bica@codurance.com>
2022-09-23 18:10:10 +0100

    Create Application class to test from command line.

---
+++ src/main/java/com/socialnetwork/Application.java
@@ -0,0 +1,37 @@
+package com.socialnetwork;
+
+import com.socialnetwork.command.CommandParser;
+import com.socialnetwork.repos.PostRepository;
+import com.socialnetwork.repos.UserRepository;
+import com.socialnetwork.service.SocialNetworkService;
+import com.socialnetwork.util.ClockService;
+import com.socialnetwork.util.Console;
+
+import java.util.Scanner;
+
+public class Application {
+
+    public static void main(String[] args) {
+
+        ClockService clockService = new ClockService();
+        Console console = new Console();
+        SocialNetworkService socialNetworkService =
+                new SocialNetworkService(new UserRepository(), new PostRepository(), clockService, console);
+        SocialNetworkAPI socialNetworkAPI =
+                new SocialNetworkAPI(new CommandParser(), socialNetworkService);
+        Scanner in = new Scanner(System.in);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> System.out.println("Exiting...")));
+
+        while (true) {
+
+            System.out.print("> ");
+            String readLine = in.nextLine();
+            if (readLine != null && !readLine.isEmpty()) {
+                socialNetworkAPI.execute(readLine);
+            }
+
+        }
+
+    }
+}
